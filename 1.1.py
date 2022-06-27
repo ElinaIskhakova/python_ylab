40e5b8b8dc720f264554c6b20b488602f248a8e4
Elina Iskhakova <35336512+ElinaIskhakova@users.noreply.github.com>
2022-06-27 22:06:05 +0500

    Update 1.1.py

--- 1.1.py
+++ 1.1.py
@@ -1,3 +1,4 @@
 #Написать метод domain_name, который вернет домен из url адреса:
 def domain_name(url):
-    return url.replace("www.","http://").split("//")[1].split(".")[0]
+    url = url.replace('https://', '').replace('http://','').replace('www.','').split('.')[0]
+    return url
