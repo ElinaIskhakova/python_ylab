0f33fad75cdfc173e84590c38c8449d7c120d297
Elina Iskhakova <35336512+ElinaIskhakova@users.noreply.github.com>
2022-06-27 22:06:39 +0500

    Update 1.4.py

--- 1.4.py
+++ 1.4.py
@@ -1,20 +1,20 @@
 #Написать метод bananas, который принимает на вход строку и
 #возвращает количество слов «banana» в строке.
 
-def solve(s, word):
-    lst = []
-    if word == '':
-        lst.append(''.rjust(len(s), '-'))
-        return lst
+import itertools
 
-    left_s = ''
-    for si in range(len(s)):
-        if word[0] == s[si]:
-            left_s = ''.rjust(si, '-') + s[si]
-            if s[si+1:] == '' and word[1:] == '':
-                lst.append(left_s)
-            else:
-                right_s_list = solve(s[si+1:], word[1:])
-                for right_s in right_s_list:
-                    lst.append(left_s + right_s)
-    return lst 
+def bananas(s):
+    result = set()
+    
+    for comb in itertools.combinations(range(len(s)), len(s) - 6):
+        arr = list(s)
+        
+        for i in comb:
+            arr[i] = '-'
+        
+        candidate = ''.join(arr)
+        
+        if candidate.replace('-', '') == 'banana':
+            result.add(candidate)
+    
+    return result
