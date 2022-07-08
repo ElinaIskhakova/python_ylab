f681a0d093b391a1e60060375edb3a388dff6336
Elina Iskhakova <35336512+ElinaIskhakova@users.noreply.github.com>
2022-07-08 13:12:37 +0500

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+
+class CyclicIterator():
+    num: list
+    index: int = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.num):
+            result = self.num[self.index]
+            self.index += 1
+        else:
+            self.index = 0
+            result = self.num[self.index]
+        return result
+
+
+if __name__ == '__main__':
+    cyclic_iterator = CyclicIterator(range(100))
+    for i in cyclic_iterator:
+        print(i)
