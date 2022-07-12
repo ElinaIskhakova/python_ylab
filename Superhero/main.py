5e2254af1690517c327d840512498b7a4873cbf9
Elina Iskhakova <35336512+ElinaIskhakova@users.noreply.github.com>
2022-07-12 22:09:01 +0500

    Add files via upload

--- Superhero/main.py
+++ Superhero/main.py
@@ -2,24 +2,16 @@ from antagonistfinder import AbstractPlace
 from heroes import Superman, ChuckNorris,TV, Newspaper, AbstractHero, AbstractNotifier
 from places import Kostroma, Tokyo, Planet
 
-
-
 class SavePlace:
-
-    def find_enemy(self):
-        return self.place.get_antagonist()
-
-    def notify(self):
-        self.notifier.publicize(f'{self.hero.name} saved the {self.place.name}!')
 
     def __init__(self, hero: AbstractHero, place: AbstractPlace, notifier: AbstractNotifier):
         self.place = place
         self.hero = hero
         self.notifier = notifier
 
-        self.find_enemy()
+        self.place.get_antagonist()
         hero.attack()
-        self.notify()
+        self.notifier.publicize(f'{self.hero.name} saved the {self.place.name}!')
         
 if __name__ == '__main__':
     SavePlace(Superman(), Kostroma(), TV())
